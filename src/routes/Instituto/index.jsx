70cd3a4348ffab1cd987fb50d7546540d6a7b237
Rafafaaa <cristofalirafael@gmail.com>
2024-03-27 19:50:08 -0300

    feat: update institute page

--- src/routes/Instituto/index.jsx
+++ src/routes/Instituto/index.jsx
@@ -5,6 +5,7 @@ import family from '../../assets/images/family.svg'
 import contact from '../../assets/images/contact.svg'
 import address from '../../assets/images/address.svg'
 import hiFive from '../../assets/images/hi-five.svg'
+import googleMaps from '../../assets/images/google-maps.png'
 
 function Instituto() {
   return (
@@ -25,14 +26,27 @@ function Instituto() {
         </div>
         <div className='container-right'>
           <h1>Contato</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, voluptatibus!</p>
+          <span>
+            <b>ICr Instituto da Criança e do Adolescente</b>
+            <p>(11) 2661-8500</p>
+            <b>Marcação de consultas – de 2ª a 6ª feira</b>
+            <p>ICr – (11) 2661-8635, das 7:00 às 16:00</p>
+            <p>ITACI – (11) 2661-8962 ou (11) 2661-8963, das 14:00 às 16:00</p>
+            <b>Agendamento de exames – de 2ª a 6ª feira</b>
+            <p>(11) 2661-8548 ou (11) 2661-8670 ou pessoalmente no 2º andar do ICr – das 11:00 às 15:00</p>
+          </span>
         </div>
       </div>
 
       <div id="address" className='container'>
         <div className='container-left'>
-          <h1>Endereço</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, consequuntur.</p>
+          <span className='flex'>
+            <a href='https://www.google.com/maps?q=Av.+Dr.+Enéas+Carvalho+de+Aguiar,+647+-+Cerqueira+César' target='_blank'>
+              <img src={googleMaps} alt="Google Maps" id="google-maps" />
+            </a>
+            <h1>Endereço</h1>
+          </span>
+          <p>Av. Dr. Enéas Carvalho de Aguiar, 647 - Cerqueira César, São Paulo - SP, 05403-000</p>
         </div>
         <div className='container-right'>
           <img src={address} alt="Endereço" />
