c207dc64899bb0eb45e5b275981256fee877ae26
Rafafaaa <cristofalirafael@gmail.com>
2024-04-02 20:56:08 -0300

    feat: responsive

--- src/routes/AreaKids/index.jsx
+++ src/routes/AreaKids/index.jsx
@@ -55,8 +55,6 @@ function AreaKids() {
               </ul>
             </div>
             <iframe
-              width="560"
-              height="315"
               src="https://www.youtube.com/embed/infocr"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               title="Vídeo YouTube"
