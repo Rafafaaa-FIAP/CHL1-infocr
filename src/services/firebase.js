d8146ffac410cca7d79ae80936d5438059175a2d
Rafafaaa <cristofalirafael@gmail.com>
2024-05-21 18:37:46 -0300

    fix: firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, query, child, get, push, set, remove } from 'firebase
 import { signInWithEmailAndPassword, signOut, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXfQ3GPnFzL7DhlHA5n6YxnxdjIHOTVmw",
-  authDomain: "infocr-fiap.firebaseapp.com",
-  databaseURL: "https://infocr-fiap-default-rtdb.firebaseio.com",
-  projectId: "infocr-fiap",
-  storageBucket: "infocr-fiap.appspot.com",
-  messagingSenderId: "958769533969",
-  appId: "1:958769533969:web:4e6abde736957cd1028db0"
+  apiKey: "AIzaSyAnGn-htAvx9hG3O_1Ah4EEewJ8S6hL4OI",
+  authDomain: "infocr-fiap-1espr.firebaseapp.com",
+  databaseURL: "https://infocr-fiap-1espr-default-rtdb.firebaseio.com",
+  projectId: "infocr-fiap-1espr",
+  storageBucket: "infocr-fiap-1espr.appspot.com",
+  messagingSenderId: "303312140900",
+  appId: "1:303312140900:web:f553c143b9f3072c59fd71"
 };
 
 const app = initializeApp(firebaseConfig);
